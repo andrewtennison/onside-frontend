31655dea3e8c705812c500474b18c8c823bdd659
Andrew Tennison <andrew@tennisons.com>
2011-11-07 18:45:04 +0000

    authentication added to node
    Signed-off-by: Andrew Tennison <andrew@tennisons.com>

--- app.js
+++ app.js
@@ -7,43 +7,32 @@ var express 	= require('express'),
 	routes 		= require('./routes'),
 	everyauth 	= require('everyauth'),
 	util 		= require('util');
-//	users    	= require('./lib/users');
 
-everyauth.twitter
-	.consumerKey('K2PPhkjulGIReJQsvhsZg')
-	.consumerSecret('37km5B9zyL2YstsPh5IcmwDqZrJDK39PMNW9ytaKl8')
-	.findOrCreateUser(function(session, accessToken, accessTokenSecret, twitterUserData){
-		var promise = this.Promise();
-		users.findOrCreateByTwitterData(twitterUserData, accessToken, accessTokenSecret, promise);
-		return promise;
-		//return usersByTwitId[twitUser.id] || (usersByTwitId[twitUser.id] = addUser('twitter', twitUser));
-	})
-	.redirectPath('/');
+// Load Authentication code - doesnt need to be set to var
+var onsideAuth = require('./lib/auth')(app);
 
-	
+// Create server	
 var app = module.exports = express.createServer();
 
 // Configuration
-
 app.configure(function(){
-  app.use(express.bodyParser());
-  app.use(express.cookieParser());
-  app.use(express.bodyParser());
-  app.use(express.session({ secret : "123456"}));
-  app.use(everyauth.middleware());
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-  everyauth.helpExpress(app);
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({ secret : "changeToSomething!"}));
+	app.use(everyauth.middleware());
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
+	everyauth.helpExpress(app);
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+	app.use(express.errorHandler()); 
 });
 
 // Routes
