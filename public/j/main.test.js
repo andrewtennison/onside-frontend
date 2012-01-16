7e3bd002409b2a518d8730f930a78c92afdc4016
Andrew Tennison <andrew@tennisons.com>
2012-01-16 11:40:28 +0000

    -
    Signed-off-by: Andrew Tennison <andrew@tennisons.com>

--- public/j/main.test.js
+++ public/j/main.test.js
@@ -17,7 +17,7 @@ on.env = {
 	docReady			: false
 };
 
-console.log('1. namespace - 0.06')
+console.log('1. namespace - 0.07')
 
 on.logger = [];
 on.helper = {
@@ -59,9 +59,12 @@ $LAB
 .script( 
 	on.path.js + 'lib/json2.js', 
 	on.path.js + 'lib/jquery-1.7.1.min.js',
-	on.path.js + 'lib/underscore-1.2.1.min.js',
+	on.path.js + 'lib/underscore-1.2.1.min.js'
+)
+.wait()
+.script(
 	on.path.js + 'lib/backbone-0.5.3.min.js',
-//	on.path.js + 'lib/pretty.js',
+	on.path.js + 'lib/pretty.js',
 //	on.path.js + 'lib/iscroll.js',
 //	on.path.js + 'lib/mbp.helper.js',
 	'http://connect.facebook.net/en_US/all.js'
