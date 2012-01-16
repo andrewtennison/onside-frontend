d80310d65858d130264e62a10c9ca76d818eb570
Andrew Tennison <andrew@tennisons.com>
2012-01-16 11:17:40 +0000

    -
    Signed-off-by: Andrew Tennison <andrew@tennisons.com>

--- public/j/main.test.js
+++ public/j/main.test.js
@@ -17,22 +17,55 @@ on.env = {
 	docReady			: false
 };
 
+console.log('1. namespace - 0.2')
+
+on.logger = [];
+on.helper = {
+	// escape HTML (prevent script insertion)
+	esc : function(string) {return string.replace(/&(?!\w+;|#\d+;|#x[\da-f]+;)/gi, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#x27;').replace(/\//g,'&#x2F;');}
+	, log : function(msg, type){
+		if (window.console === undefined || on.env.server !== 'development'){
+			on.logger.push(type +' = '+ msg);
+		} else {
+			try {
+				if(type === undefined) type = 'log';
+				console[type](msg);
+			} catch(err){
+				console.error(err)
+				console.log(msg)
+			};
+		}
+	}
+}
 
 on.path = {
-	js: '/j/'
+	js: '/j/',
+	api:'/api',
+	facebookCss: '//dev.onside.me:3000/c/facebook.css?1'
+};
+
+window.fbAsyncInit = function() {
+	FB.init({
+		appId		: '266299360074356',
+		channelUrl	: '//dev.onside.me:3000/fb_channel',
+		status		: true, // check login status
+		cookie		: true, // enable cookies to allow the server to access the session
+		xfbml		: true	// parse XFBML
+	});
 };
 
 
 $LAB
 .script( 
-//	on.path.js + 'lib/json2.js', 
-	on.path.js + 'lib/jquery-1.7.1.min.js'
-//	on.path.js + 'lib/underscore-1.2.1.min.js',
-//	on.path.js + 'lib/backbone-0.5.3.min.js',
-//	on.path.js + 'lib/pretty.js',
-//	on.path.js + 'lib/iscroll.js',
-//	on.path.js + 'lib/mbp.helper.js',
-//	'http://connect.facebook.net/en_US/all.js'
+	// put in head, use instead of lib.js & user modernizr.load() - libs/modernizr-custom.js
+	on.path.js + 'lib/json2.js', 
+	on.path.js + 'lib/jquery-1.7.1.min.js',
+	on.path.js + 'lib/underscore-1.2.1.min.js',
+	on.path.js + 'lib/backbone-0.5.3.min.js',
+	on.path.js + 'lib/pretty.js',
+	on.path.js + 'lib/iscroll.js',
+	on.path.js + 'lib/mbp.helper.js',
+	'http://connect.facebook.net/en_US/all.js'
 )
 .wait(function(){
 	console.log('3. models + collections')
